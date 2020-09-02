25ce6a931d117c8829238d96a219bbe0aa29f5d5
uriha <urihanunov@gmail.com>
2020-09-02 16:11:15 +0300

    Add counters for different length of contigs

--- contigs_analysis.py
+++ contigs_analysis.py
@@ -6,7 +6,9 @@ def some_function(name, sequence):
 
 contigs_file = "../contigs-outputs/basic_k-mer24/basic_try_k-mer24.contigs.fa"
 fasta_sequences = SeqIO.parse(open(contigs_file),'fasta')
-counter = 0
+counterOfContigs = 0
+counterOfBigContigs = 0
+counterOfHugeContigs = 0
 min_length = float('inf')
 max_length = float('-inf')
 sum_length = 0
@@ -14,7 +16,7 @@ for fasta in fasta_sequences:
     name, sequence = fasta.id, str(fasta.seq)
     #new_sequence = some_function(name, sequence)
     # count  the number of contigs
-    counter = counter + 1
+    counterOfContigs = counterOfContigs + 1
     contig_len = len(sequence)
     # sum the length of the contigs:
     sum_length = sum_length + contig_len
@@ -23,11 +25,19 @@ for fasta in fasta_sequences:
         min_length = contig_len
     if contig_len > max_length:
         max_length = contig_len
+    # count the number of contigs with length of 150 - 300
+    if contig_len > 150 and contig_len <= 300:
+        counterOfBigContigs += 1
+    # count the number of contigs with length of over 300
+    if contig_len > 300:
+        counterOfHugeContigs += 1
 
 # find the avarege length of contigs
 avg = sum_length / counter
 
-print("number of contigs:", counter)
+print("number of contigs:", counterOfContigs)
 print("min length of contig:", min_length)
 print("max length of contig:", max_length)
 print("avarege length of contigs:", avg)
+print("number of contigs with length of 150 - 300:", counterOfBigContigs)
+print("number of contigs with length of over 300:", counterOfHugeContigs)
