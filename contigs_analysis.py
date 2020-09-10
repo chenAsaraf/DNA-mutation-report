57bf19dcd610dd7e61b05e84e3e51e93f00b37a3
chenAsaraf <chen21asr@gmail>
2020-09-10 13:23:47 +0000

    add counter of reads

--- contigs_analysis.py
+++ contigs_analysis.py
@@ -2,18 +2,16 @@ from Bio import SeqIO
 import numpy as np
 import matplotlib.pyplot as plt
 
-def some_function(name, sequence):
-    print(name, sequence)
 
-def analayse_histogram(array_of_lengthes):
-    bins = np.concatenate(([100,200,300,400,500,1000],np.arange(2000,30000,1000)), axis=None)
-    hist, bin_edges = np.histogram(array_of_lengthes, bins = bins)
+def plot_histogram(array_of_lengthes):
+    bins = np.concatenate(([100,200,300,400,500,1000],np.arange(2000,6000,1000)), axis=None)
     fig = plt.figure()
-    plt.hist(array_of_lengthes, bins = bins)
-    plt.title("Histogram of contgis lengthes")
-    #plt.show()
-    fig.savefig('histogram_basic24.png')
-    return hist, bin_edges
+    plt.hist(array_of_lengthes, bins=bins, density=True, facecolor='g')
+    plt.title("Histogram of contgis lengthes - with k-mer : 24 for R2")
+    plt.xlabel('Length (bp)')
+    plt.grid(True)
+    fig.savefig('histogram_basic24_R2.png')
+
 
 def read_fasta(contigs_file):
     fasta_sequences = SeqIO.parse(open(contigs_file),'fasta')
@@ -24,7 +22,6 @@ def read_fasta(contigs_file):
     list_of_length =[]
     for fasta in fasta_sequences:
         name, sequence = fasta.id, str(fasta.seq)
-        #new_sequence = some_function(name, sequence)
         # count  the number of contigs
         counter = counter + 1
         contig_len = len(sequence)
@@ -42,11 +39,17 @@ def read_fasta(contigs_file):
     print("min length of contig:", min_length)
     print("max length of contig:", max_length)
     print("avarege length of contigs:", avg)"""
-    hist, bin_edges = analayse_histogram(np.array(list_of_length))
-    #print(list_of_length)
+    plot_histogram(np.array(list_of_length))
     """print("histogram:")
     print(hist)
     print("bin_edges:")
     print(bin_edges)"""
 
-read_fasta("../contigs-outputs/basic_k-mer24/basic_try_k-mer24.contigs.fa")
+def read_source_file(reads_file):
+	count = 0
+	for record in SeqIO.parse(open(reads_file), "fastq-illumina"):
+		count = count + 1
+	print("the number of reads are : ", count)
+
+read_fasta("../contigs-outputs/basic_k-mer24-R2/basic_k-mer24-R2.contigs.fa")
+read_source_file("../source_files+minia/sample_TB0001955-16933-N_R1_001.fastq") 
