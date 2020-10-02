f5cffb3b7122e6e62d7b55f768cf7e254c64386a
Uri Hanunov <44783443+UriHanunov@users.noreply.github.com>
2020-10-03 02:22:05 +0300

    Update mutations_distance.py
    Add the PointMutation class and some features for the report

--- main-code/mutations_distance.py
+++ main-code/mutations_distance.py
@@ -1,56 +1,76 @@
 import edit_distance
 from matplotlib import pyplot as plt
-import numpy as np
+import random
 
-#edit distance between 2 contigs
-def editDistance(str1, str2):
-    # mistake = len(str1)/10
-    sm = edit_distance.SequenceMatcher(a=str1, b=str2)
-    # if sm.distance() < mistake:
-    f = open("similarContigs.txt", "a")
-    f.write(str1 + "\n" + str2 + "\n")
-    f.write("The edit distance of this contigs: " + str(sm.distance()) + "\n")
-    f.write("The matches of this contigs: " + str(sm.matches()) + "\n")
-    temp = [0, 0, 0]
-    temp2 = ["The inserts are: ", "The replaces are: ", "The deletes are: "]
-    for i in sm.get_opcodes():
-        if i[0] == "insert":
-            temp[0] += 1
-            temp2[0] = temp2[0] + str2[i[3]:i[4]] + ", "
-        if i[0] == "replace":
-            temp[1] += 1
-            temp2[1] = temp2[1] + str2[i[3]:i[4]] + "->" + str1[i[1]:i[2]] + ", "
-        if i[0] == "delete":
-            temp[2] += 1
-            temp2[2] = temp2[2] + str1[i[1]:i[2]] + ", "
-    if temp[0] > 0:
-        f.write("Number of inserts: " + str(temp[0]) + ". " + str(temp2[0][0:len(temp2[0]) - 2]) + "\n")
-    else:
-        f.write("Number of inserts: " + str(temp[0]) + ". \n ")
-    if temp[1] > 0:
-        f.write("Number of replaces: " + str(temp[1]) + ". " + str(temp2[1][0:len(temp2[1])-2]) + "\n")
-    else:
-        f.write("Number of replaces: " + str(temp[1]) + ". \n ")
-    if temp[2] > 0:
-        f.write("Number of deletes: " + str(temp[2]) + ". " + str(temp2[2][0:len(temp2[2])-2]) + "\n")
-    else:
-        f.write("Number of deletes: " + str(temp[2]) + ". \n ")
-    f.close()
-    editDistance.counters[0] = temp[0] / len(str1)
-    editDistance.counters[1] = temp[1] / len(str1)
-    editDistance.counters[2] = temp[2] / len(str1)
-    editDistance.counters[3] = sm.matches() / len(str1)
+#
+#
+#
+class PointMutation:
+    def __init__(self):
+        self.counters = [0, 0, 0, 0]
+        self.counterOfCompares = 0
+        self.sumOfLength = 0
+        bases = ('A', 'C', 'G', 'T')
+        replace = ('AC', 'AG', 'AT', 'CA', 'CG', 'CT', 'GA', 'GC', 'GT', 'TA', 'TC', 'TG')
+        self.inserts = dict.fromkeys(bases, 0)
+        self.deletes = dict.fromkeys(bases, 0)
+        self.replaces = dict.fromkeys(replace, 0)
 
-editDistance.counters = [0,0,0,0]
-str1 = "saturidayi"
-str2 = "saturdyj"
-editDistance(str1,str2)
-print(editDistance.counters)
-# Creating plot
-fig = plt.figure(figsize=(10, 7))
-plt.pie(editDistance.counters, labels=["inserts", "replaces", "deletes", "matches"], autopct='%1.1f%%')
+    def editDistance(self, tumor, healthy):
+        mistake = len(tumor)/10  # 10% of mistakes
+        detailsOfDistance = edit_distance.SequenceMatcher(a=tumor, b=healthy)
+        if detailsOfDistance.distance() < mistake:
+            self.counterOfCompares += 1
+            self.sumOfLength += len(tumor)
+            counters = [0, 0, 0]  # counters of: inserts, replaces and deletes
+            theChanges = ["The inserts are: ", "The replaces are: ", "The deletes are: "]  # the changes of the Mutations
+            for i in detailsOfDistance.get_opcodes():
+                if i[0] == "insert":
+                    counters[0] += 1
+                    theChanges[0] = theChanges[0] + healthy[i[3]:i[4]] + ", "
+                    self.inserts[healthy[i[3]:i[4]]] = self.inserts[healthy[i[3]:i[4]]] + 1
+                if i[0] == "replace":
+                    counters[1] += 1
+                    theChanges[1] = theChanges[1] + healthy[i[3]:i[4]] + "->" + tumor[i[1]:i[2]] + ", "
+                    temp = healthy[i[3]:i[4]] + tumor[i[1]:i[2]]
+                    self.replaces[temp] = self.replaces[temp] + 1
+                if i[0] == "delete":
+                    counters[2] += 1
+                    theChanges[2] = theChanges[2] + tumor[i[1]:i[2]] + ", "
+                    temp = str(tumor[i[1]:i[2]])
+                    self.deletes[temp] = self.deletes[temp] + 1
+            if random.choice(range(1, 101)) > 90:  # take 1/10 from the Mutations to the report
+                f = open("similarContigs.txt", "a")
+                f.write(tumor + "\n" + healthy + "\n")
+                f.write("The edit distance of this contigs: " + str(detailsOfDistance.distance()) + "\n")
+                f.write("The matches of this contigs: " + str(detailsOfDistance.matches()) + "\n")
+                if counters[0] > 0:
+                    f.write("Number of inserts: " + str(counters[0]) + ". " + str(theChanges[0][0:len(theChanges[0]) - 2]) + "\n")
+                else:
+                    f.write("Number of inserts: " + str(counters[0]) + ". \n")
+                if counters[1] > 0:
+                    f.write("Number of replaces: " + str(counters[1]) + ". " + str(theChanges[1][0:len(theChanges[1])-2]) + "\n")
+                else:
+                    f.write("Number of replaces: " + str(counters[1]) + ". \n")
+                if counters[2] > 0:
+                    f.write("Number of deletes: " + str(counters[2]) + ". " + str(theChanges[2][0:len(theChanges[2])-2]) + "\n")
+                else:
+                    f.write("Number of deletes: " + str(counters[2]) + ". \n")
+                f.close()
+            self.counters[0] += counters[0] / len(tumor)
+            self.counters[1] += counters[1] / len(tumor)
+            self.counters[2] += counters[2] / len(tumor)
+            self.counters[3] += detailsOfDistance.matches() / len(tumor)
 
-# show plot
-plt.show()
-
-
+a = PointMutation()
+str1 = "AGCTAGCGCTCGTCGT"
+str2 = "AGCTGGGGTTTGTGAA"
+a.editDistance(str1, str2)
+print(a.counters)
+print(a.replaces)
+# # Creating plot
+# fig = plt.figure(figsize=(10, 7))
+# plt.pie(a.counters, labels=["inserts", "replaces", "deletes", "matches"], autopct='%1.1f%%')
+#
+# # show plot
+# plt.show()
