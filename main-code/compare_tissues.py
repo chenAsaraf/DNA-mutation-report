358339d4bbea2a7b8ccc051357fa3408570ffb01
Uri Hanunov <chen21asr@gmail.com>
2020-10-03 23:33:05 +0000

    Fix bug of strings in compare_tissues

--- main-code/compare_tissues.py
+++ main-code/compare_tissues.py
@@ -4,6 +4,7 @@ from build_dictionary import tissueDictionary
 from matplotlib import pyplot as plt
 from mutations_distance import PointMutation
 from contigs_analysis import filter_contigs_by_size
+import time
 # 1) parse the tumor_cell with non-overlaping window and find there bucket in the dictionary
 # 2) search for each contig in the bucket-list in the BST
 # 3) find alignment (maby another function)
@@ -29,7 +30,7 @@ def find_similar_section(tumor_file, k, dictionary, healthyStorage, test=False, 
                     healthy_seq = healthyStorage[record.id]
                     # For each alignment - find the overlapping parts and send to the Edit-Distance function
                     for healthy_idx in record.indexes:
-                        healthy, tumor = find_overlap(healthy_seq, tumor_seq, healthy_idx, window)
+                        healthy, tumor = find_overlap(str(healthy_seq), str(tumor_seq.seq), healthy_idx, window)
                         mutations_report.editDistance(tumor,healthy)
     return mutations_report
 
@@ -59,26 +60,38 @@ def find_overlap(healthy_seq, tumor_seq, healthy_idx, tumor_idx):
     else:
         end_healthy = healthy_idx + remaining_tumor
         end_tumor = tumor_idx + remaining_tumor
-    return str(healthy_seq)[begin_healthy : end_healthy], str(tumor_seq)[begin_tumor : end_tumor]
+    return healthy_seq[begin_healthy : end_healthy], tumor_seq[begin_tumor : end_tumor]
 
 
 def compare_tissues(healthy_file, tumor_file, test=False, test_num=1000):
+    start_time_build_dict = time.time()
+
     if test:
         dictBuilder = tissueDictionary(healthy_file, test=True, test_num=test_num)
     else:
         dictBuilder = tissueDictionary(healthy_file)
+
+    print("--- %s seconds to BUILD dictionary ---" % (time.time() - start_time_build_dict))
+
     dictionary, contigsStorage = dictBuilder.get_dictionary_and_storage()
     k = dictBuilder.getK()
 
+    start_time_compare = time.time()
+
     mutations_report = find_similar_section(tumor_file, k, dictionary, contigsStorage, test=test, test_num=test_num)
-    print(mutations_report.inserts)
-    print(mutations_report.replaces)
-    print(mutations_report.deletes)
-    print(mutations_report.counterOfCompares)
-    print(mutations_report.sumOfLength/mutations_report.counterOfCompares)
+
+    print("--- %s seconds to COMPARE all tissues ---" % (time.time() - start_time_compare))
+
+    print()
+    print("MUTATIONS REPORT:")
+    print("INSERTS:", mutations_report.inserts)
+    print("REPLACES:", mutations_report.replaces)
+    print("DELETES:", mutations_report.deletes)
+    print("NUMBER OF CONTIGS WHERE COMPARED:", mutations_report.counterOfCompares)
+    print("AVG LENGTH OF CONTIG:", mutations_report.sumOfLength/mutations_report.counterOfCompares)
     # Creating plot
     fig = plt.figure(figsize=(10, 7))
     plt.pie(mutations_report.counters, labels=["inserts", "replaces", "deletes", "matches"], autopct='%1.1f%%')
 
     # save plot
-    fig.savefig('pie_Of_100000_mutations.png')
+    fig.savefig('pie_Of_1000_mutations.png')
