6a9e6a1f6007762f96345f461e8d1f8d3cdfbfff
Uri Hanunov <44783443+UriHanunov@users.noreply.github.com>
2020-10-03 13:23:52 +0300

    Update test_compare_tissues.py
    Fix the path of files

--- main-code/test_compare_tissues.py
+++ main-code/test_compare_tissues.py
@@ -45,8 +45,8 @@ def test_3_find_overlap():
 Test: compare_tissues
 """
 def test_compare_tissues():
-    healthy = "../contigs-outputs/healthy/basic_k-mer24/basic_try_k-mer24.contigs.fa"
-    tumor = "../contigs-outputs/tumor/basic_k-mer24_T/basic_k-mer24_T.contigs.fa"
+    healthy = "../../contigs-outputs/healthy/basic_k-mer24/basic_try_k-mer24.contigs.fa"
+    tumor = "../../contigs-outputs/tumor/basic_k-mer24_T/basic_k-mer24_T.contigs.fa"
     compare_tissues(healthy, tumor, test=True)
 
 """test_1_find_overlap()
