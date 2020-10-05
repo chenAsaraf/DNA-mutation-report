86895c97898e383e8db881637012ea9a4c4f2605
chenAsaraf <chen21asr@gmail.com>
2020-10-05 19:32:53 +0300

    Change the test to fit home computer

--- main-code/test_compare_tissues.py
+++ main-code/test_compare_tissues.py
@@ -47,9 +47,17 @@ Test: compare_tissues
 """
 def test_compare_tissues():
     healthy = "sample_contigs_k24.contigs.fa"
-    tumor = "sample_contigs_k24_T.contigs.fa"
-    output_prefix = sys.argv[2]
-    test_num = int(sys.argv[1])
+    tumor = "sample_contigs_k24_tumor.contigs.fa"
+    argvlen = len(sys.argv)
+    if argvlen > 1:
+        test_num = int(sys.argv[1])
+    else:
+        test_num = 100
+        output_prefix = "output"
+    if argvlen > 2:
+        output_prefix = sys.argv[2]
+    else: output_prefix = "output"
+
     compare_tissues(healthy, tumor, output_prefix, test=True, test_num=test_num)
 
 """test_1_find_overlap()
