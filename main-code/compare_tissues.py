c6994d2de1c21840d82b472d4d2bae2ca8eafa64
Uri Hanunov <44783443+UriHanunov@users.noreply.github.com>
2020-10-05 23:03:31 +0300

    Fix the pie chart

--- main-code/compare_tissues.py
+++ main-code/compare_tissues.py
@@ -122,13 +122,19 @@ def compare_tissues(healthy_file, tumor_file, output_prefix, test=False, test_nu
     print("~ Number of contigs compared:", mutations_report.counterOfComparisons)
 
     # Creating plot
-    fig = plt.figure(figsize=(10, 7))
+    total = plt.figure(figsize=(10, 7))
     plt.pie([inserts_amount, replaces_amount, deletes_amount], labels=["inserts", "replaces", "deletes"], autopct='%1.1f%%')
+    inserts = plt.figure(figsize=(10, 7))
+    plt.pie(list(mutations_report.inserts.values()), labels=list(mutations_report.inserts.keys()), autopct='%1.1f%%')
+    replaces = plt.figure(figsize=(10, 7))
+    plt.pie(list(mutations_report.replaces.values()), labels=list(mutations_report.replaces.keys()), autopct='%1.1f%%')
+    deletes = plt.figure(figsize=(10, 7))
+    plt.pie(list(mutations_report.deletes.values()), labels=list(mutations_report.deletes.keys()), autopct='%1.1f%%')
+    
     # save plot
-    fig.savefig(output_prefix + "-total.png")
-    plt.pie([mutations_report.inserts["A"], mutations_report.inserts["C"], mutations_report.inserts["G"], mutations_report.inserts["T"]], labels=["A", "C", "G", "T"], autopct='%1.1f%%')
-    fig.savefig(output_prefix + "-inserts.png")
-    plt.pie([mutations_report.deletes["A"], mutations_report.deletes["C"], mutations_report.deletes["G"], mutations_report.deletes["T"]], labels=["A", "C", "G", "T"], autopct='%1.1f%%')
-    fig.savefig(output_prefix + "-deletes.png")
+    total.savefig(output_prefix + "-total.png")
+    inserts.savefig(output_prefix + "-inserts.png")
+    replaces.savefig(output_prefix + "-replaces.png")
+    deletes.savefig(output_prefix + "-deletes.png")
     
     
