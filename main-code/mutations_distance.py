5f753904f516cb799fdbaf7be6c88cc68490e011
chenAsaraf <chen21asr@gmail.com>
2020-10-05 19:09:37 +0300

    Change names, rearrange printing

--- main-code/mutations_distance.py
+++ main-code/mutations_distance.py
@@ -2,7 +2,7 @@ import edit_distance
 import random
 import math
 from Bio import pairwise2
-
+from enum import Enum
 
 """
 This modulo is based on the 'edit_distance' library, that implements the Levenshtein distance.
@@ -10,45 +10,69 @@ Informally, the Levenshtein distance between two words is the minimum number of 
 (insertions, deletions or substitutions) required to change one word into the other (wikipedia)
 To be noted: The 'edit_distance' library implementation could likely be optimized to be faster
 within Python. And could probably be much faster if implemented in C.
+
+We used the following documentation:
+https://docs.python.org/2/library/difflib.html
 """
+INSERTS = 0
+REPLACES = 1
+DELETES = 2
 
 class PointMutation:
+
     def __init__(self, output_prefix):
         self.output_prefix = output_prefix
-        self.counters = [0, 0, 0, 0]  # counters for each mutation types: inserts, replaces, deletes and matches
-        self.counterOfCompares = 0  # number of compared contigs
-        self.sumOfLength = 0  # sum length of all the compared contigs (for avg)
-        bases = ('A', 'C', 'G', 'T')  # the keys for the Dictionary of inserts and deletes
-        replace = ('AC', 'AG', 'AT', 'CA', 'CG', 'CT', 'GA', 'GC', 'GT', 'TA', 'TC', 'TG')  # the keys for the Dictionary of replaces
+        # self.counters = [0, 0, 0, 0]  # counters for each mutation types: inserts, replaces, deletes and matches
+        self.counterOfComparisons = 0  # number of compared contigs
+        self.sumOfLengths = 0  # sum lengths of all the compared contigs (for avg)
+        # the keys for the Dictionary of inserts and deletes:
+        bases = ('A', 'C', 'G', 'T')
+        # the keys for the Dictionary of replaces
+        replace = ('AC', 'AG', 'AT', 'CA', 'CG', 'CT', 'GA', 'GC', 'GT', 'TA', 'TC', 'TG')
         # set all the values in the Dictionary to 0
         self.inserts = dict.fromkeys(bases, 0)
         self.deletes = dict.fromkeys(bases, 0)
         self.replaces = dict.fromkeys(replace, 0)
 
-    def editDistance(self, tumor, healthy):
+    def editDistance(self, healthy, tumor):
         errors_precent = math.ceil(len(tumor)/10)  # 10% of mistakes
-        sequence_match = edit_distance.SequenceMatcher(a=tumor, b=healthy)
+        # The first string (i.e 'a') is the one that is compared to the second.
+        # The second string (i.e 'b') does not change.
+        sequence_match = edit_distance.SequenceMatcher(a=healthy, b=tumor)
         distance = sequence_match.distance()  # distance between the contigs
         matches = sequence_match.matches()  # the matches (the same chars) between the contigs
 
         if distance < errors_precent:
-            self.counterOfCompares += 1
-            self.sumOfLength += len(tumor)
-            temp_counters = [0, 0, 0]  # counters of: inserts, replaces and deletes
-            theChanges = ["The inserts are: ", "The replaces are: ", "The deletes are: "]  # the changes of the Mutations
+            self.counterOfComparisons += 1
+            self.sumOfLengths += len(tumor)
+            # counters of: inserts, replaces and deletes
+            counters_for_mutations = [0, 0, 0]
+            # Strings for later printed samples:
+            TheInserts = "Actual inserts: "
+            TheReplaces = "Actual replaces: "
+            TheDeletes = "Actual deletes: "
+
             for opcode in sequence_match.get_opcodes():  # Return list of 5-tuples describing how to turn a into b
                 if opcode[0] == "insert":
-                    temp_counters[0] += 1
-                    theChanges[0] = theChanges[0] + healthy[opcode[3]:opcode[4]] + ", "  # Add the char that we insert to the tumor contig
-                    self.inserts[healthy[opcode[3]:opcode[4]]] += 1  # Add 1 to the values of this key
+                    counters_for_mutations[INSERTS] += 1
+                    inserted_char = tumor[opcode[3]:opcode[4]]
+                    # Add the char that we insert to the tumor contig
+                    TheInserts = TheInserts + inserted_char + ", "
+                    # Increase the char-entry in the dictionary
+                    self.inserts[inserted_char] += 1
                 if opcode[0] == "replace":
-                    temp_counters[1] += 1
-                    theChanges[1] = theChanges[1] + healthy[opcode[3]:opcode[4]] + "->" + tumor[opcode[1]:opcode[2]] + ", "
-                    self.replaces[healthy[opcode[3]:opcode[4]] + tumor[opcode[1]:opcode[2]]] += 1  # Add 1 to the values of this key
+                    counters_for_mutations[REPLACES] += 1
+                    replaced_from = healthy[opcode[1]:opcode[2]]
+                    replaced_to = tumor[opcode[3]:opcode[4]]
+                    TheReplaces = TheReplaces + replaced_from + "->" + replaced_to + ", "
+                    # Increase the chars-entry in the dictionary
+                    self.replaces[replaced_from + replaced_to] += 1
                 if opcode[0] == "delete":
-                    temp_counters[2] += 1
-                    theChanges[2] = theChanges[2] + tumor[opcode[1]:opcode[2]] + ", "
-                    self.deletes[tumor[opcode[1]:opcode[2]]] += 1  # Add 1 to the values of this key
+                    counters_for_mutations[DELETES] += 1
+                    deleted_char = healthy[opcode[1]:opcode[2]]
+                    TheDeletes = TheDeletes + deleted_char + ", "
+                    # Increase the chars-entry in the dictionary
+                    self.deletes[deleted_char] += 1
             if random.choice(range(1, 101)) > 90 and distance > 0:  # Take 1/10 from the compared contigs to the sampling report in term that there are at least 1 mutation between them
                 f = open(self.output_prefix + ".txt", "a")  # The sampling report
                 f.write("\n============================================================================\n")
@@ -57,20 +81,20 @@ class PointMutation:
                 # f.write("Edit Distance: " + str(distance) + "\n")
                 # f.write("The matches of this contigs: " + str(sequence_match.matches()) + "\n")
                 # For each mutation type, check if there are mutations like this type and add to the sampling report with the mutations themselves
-                if temp_counters[0] > 0:
-                    f.write("Inserts: " + str(temp_counters[0]) + ". " + str(theChanges[0][0:len(theChanges[0]) - 2]) + "\n")
+                if counters_for_mutations[INSERTS] > 0:
+                    f.write("Inserts Amount: " + str(counters_for_mutations[INSERTS]) + ". " + TheInserts + "\n")
                 else:
-                    f.write("Inserts: " + str(temp_counters[0]) + ". \n")
-                if temp_counters[1] > 0:
-                    f.write("Replaces: " + str(temp_counters[1]) + ". " + str(theChanges[1][0:len(theChanges[1])-2]) + "\n")
+                    f.write("Inserts Amount: " + str(counters_for_mutations[INSERTS]) + ". \n")
+                if counters_for_mutations[REPLACES] > 0:
+                    f.write("Replaces Amount: " + str(counters_for_mutations[REPLACES]) + ". " + TheReplaces + "\n")
                 else:
-                    f.write("Replaces: " + str(temp_counters[1]) + ". \n")
-                if temp_counters[2] > 0:
-                    f.write("Deletes: " + str(temp_counters[2]) + ". " + str(theChanges[2][0:len(theChanges[2])-2]) + "\n")
+                    f.write("Replaces Amount: " + str(counters_for_mutations[REPLACES]) + ". \n")
+                if counters_for_mutations[DELETES] > 0:
+                    f.write("Deletes Amount: " + str(counters_for_mutations[DELETES]) + ". " + TheDeletes + "\n")
                 else:
-                    f.write("Deletes: " + str(temp_counters[2]) + ". \n")
+                    f.write("Deletes Amount: " + str(counters_for_mutations[DELETES]) + ". \n")
                 f.close()
-            self.counters[0] += temp_counters[0]  # Add the part of inserts in the contig
-            self.counters[1] += temp_counters[1]  # Add the part of replaces in the contig
-            self.counters[2] += temp_counters[2]  # Add the part of deletes from the contig
+            self.counters[0] += counters_for_mutations[0]  # Add the part of inserts in the contig
+            self.counters[1] += counters_for_mutations[1]  # Add the part of replaces in the contig
+            self.counters[2] += counters_for_mutations[2]  # Add the part of deletes from the contig
             self.counters[3] += matches  # Add the part of matches between the contigs
