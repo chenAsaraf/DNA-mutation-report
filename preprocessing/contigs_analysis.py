372ffd3a91fb83f8e569088fb76af2ae90602640
Uri Hanunov <chen21asr@gmail.com>
2020-10-08 09:48:50 +0000

    Fix bug in histogram bins

--- preprocessing/contigs_analysis.py
+++ preprocessing/contigs_analysis.py
@@ -4,13 +4,15 @@ import matplotlib.pyplot as plt
 
 
 def plot_histogram(array_of_lengthes):
-    bins = np.concatenate(([50,100,200,300,400,500,1000],np.arange(2000,9000,1000)), axis=None)
+    bins = np.concatenate(([0,100,200,300,400,500,1000],np.arange(2000,10000,1000)), axis=None)
     fig = plt.figure()
-    plt.hist(array_of_lengthes, bins=bins, density=True, facecolor='g')
+    plt.hist(array_of_lengthes, bins=bins, facecolor='g')
     plt.title("Histogram of Normal Cell Contigs (kmer:24)")
-    plt.xlabel('Length (bp)')
-    plt.grid(True)
-    fig.savefig('histogram-normal-contigs-k24_1.png')
+    plt.yscale("log")
+    #plt.grid(True)
+    plt.ylabel('Length (bp)')
+    plt.ylabel('# of Contigs')
+    fig.savefig('histogram-normal-contigs-k24_4.png')
 
 
 def analyse_file(file, source=False):
@@ -44,19 +46,13 @@ def analyse_file(file, source=False):
     print("min length of sequence:", min_length)
     print("max length of sequence:", max_length)
     print("avarege length of sequence:", avg)
-    bins = np.concatenate(([100,200,300,400,500,1000],np.arange(2000,9000,1000)), axis=None)
+    bins = np.concatenate(([0,100,200,300,400,500,1000],np.arange(2000,10000,1000)), axis=None)
     hist, bin_edges = np.histogram(np.array(list_of_length), bins=bins)
     print("histogram:")
     print(hist)
     print("bin_edges:")
     print(bin_edges)
-    #plot_histogram(np.array(list_of_length))
-
-def read_source_file(reads_file):
-	count = 0
-	for record in SeqIO.parse(open(reads_file), 'fastq'):
-		count = count + 1
-	print("the number of reads are : ", count)
+    plot_histogram(np.array(list_of_length))
 
 
 def find_window_size(records):
@@ -66,7 +62,7 @@ def find_window_size(records):
         if seq_len < min_length:
             min_length = seq_len
 
-#analyse_file("../contigs-outputs/basic_k-mer24/basic_try_k-mer24.contigs.fa")
+analyse_file("../../contigs-outputs/healthy/basic_k-mer24/basic_try_k-mer24.contigs.fa")
 #analyse_file("../source_files+minia/sample_TB0001955-16933-N_R1_001.fastq", source=True)
 #analyse_file("../main-code/sample_contigs_k24.contigs.fa")
-#read_source_file("../source_files+minia/sample_TB0001955-16933-N_R1_001.fastq") 
+
