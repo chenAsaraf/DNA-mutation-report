efe792fb26d4212e4df25fe2fa8b2c1e1f4f6aa0
chenAsaraf <44566578+chenAsaraf@users.noreply.github.com>
2020-10-11 20:35:59 +0300

    Documaetnation

--- main-code/test_compare_tissues.py
+++ main-code/test_compare_tissues.py
@@ -2,25 +2,32 @@ from compare_tissues import compare_tissues
 import sys
 
 
-"""
-Test: compare_tissues
-"""
-def test_compare_tissues():
+def run_compare_tissues():
+    """run compare_tissues
+    
+    used:
+    python3.6 compare_tissues helathy_file_path tumor_file_path output_prefix (optional) test_num (optional)
+    helathy_file_path, tumor_file_path - contigs file in FASTA format
+    test_num 
+    
+    """
 
     healthy = "../../contigs-outputs/healthy/basic_k-mer24/basic_try_k-mer24.contigs.fa"
     tumor = "../../contigs-outputs/tumor/basic_k-mer24_T/basic_k-mer24_T.contigs.fa"
     argvlen = len(sys.argv)
-
-    if argvlen > 1:
-        output_prefix = sys.argv[1]
+    if argvlen < 3:  # missing contigs file path
+        # raise error
     else:
+        healthy = sys.argv[1]
+        tumor = sys.argv[2]
+    if argvlen > 3:
+        output_prefix = sys.argv[3]
+    else: 
         output_prefix = "output"
-        test_num = 100
-    if argvlen > 2:
-        test_num = sys.argv[2]
+    if argvlen > 4:
+        test_num = sys.argv[4]
     else:
         test_num = None
-
 
     compare_tissues(healthy, tumor, output_prefix, test=False, test_num=test_num)
 
